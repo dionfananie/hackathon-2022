373a6dacbdd53c83f03d6cdf00f21202a183f5ea
Kevin Giovanni <kevinevraldo@gmail.com>
2022-01-14 13:43:01 +0700

    fix regex test

--- src/utils/textProcessing.js
+++ src/utils/textProcessing.js
@@ -1,34 +1,34 @@
-const regexBPJS = new RegExp(/(.*bayar bpjs.*|.*tagihan bpjs.*)/g);
-const regexPLN = new RegExp(/(.*tagihan listrik.*|.*token listrik.*|.*pln.*|.*pulsa listrik.*)/g);
-const regexPulsa = new RegExp(/(.*beli pulsa.*|.*pulsa telkomsel.*|.*pulsa xl.*|.*pulsa indosat.*|.*pulsa simpati.*)/g);
-const regexPaketData = new RegExp(/(.*paket data.*)/g);
-const regexFlightSchedule = new RegExp(/(.*tiket pesawat.*)|(.*tiket garuda.*)|(.*air asia.*)|(.*lion air.*)|(.*citilink.*)/g);
-const regexTrainSchedule = new RegExp(/(.*tiket kereta.*)|(.*tiket kai.*)|(.*naik kereta.*)|(.*kereta api.*)|(.*taksaka.*)|(.*argolawu.*)|(.*malabar.*)|(.*argodwipangga.*)/g);
-const regexEmoney = new RegExp(/(.*emoney.*)|(.*e-money.*)|(.*uang elektronik.*)|(.*flazz.*)|(.*kartu tol.*)/g);
-const regexInstallment = new RegExp(/(.*bayar cicilan.*)|(.*pinjaman.*)|(.*bayar agsuran.*)/g);
-const regexPBB = new RegExp(/(.*pbb.*)|(.*pajak bumi bangunan.*)|(.*pajak bumi dan bangunan.*)/g);
-const regexVoucherGame = new RegExp(/(.*cash pb.*)|(.*point blank.*)|(.*genshin impact.*)|(.*genshin.*)|(.*steam wallet.*)|(.*mole.*)|(.*mobile legend.*)|(.*genshin.*)|(.*voucher game.*)/g);
+const regexBPJS = new RegExp(/(.*bayar bpjs.*|.*tagihan bpjs.*)/ig);
+const regexPLN = new RegExp(/(.*tagihan listrik.*|.*token listrik.*|.*pln.*|.*pulsa listrik.*)/ig);
+const regexPulsa = new RegExp(/(.*beli pulsa.*|.*pulsa telkomsel.*|.*pulsa xl.*|.*pulsa indosat.*|.*pulsa simpati.*)/ig);
+const regexPaketData = new RegExp(/(.*paket data.*)/ig);
+const regexFlightSchedule = new RegExp(/(.*tiket pesawat.*)|(.*tiket garuda.*)|(.*air asia.*)|(.*lion air.*)|(.*citilink.*)/ig);
+const regexTrainSchedule = new RegExp(/(.*tiket kereta.*)|(.*tiket kai.*)|(.*naik kereta.*)|(.*kereta api.*)|(.*taksaka.*)|(.*argolawu.*)|(.*malabar.*)|(.*argodwipangga.*)/ig);
+const regexEmoney = new RegExp(/(.*emoney.*)|(.*e-money.*)|(.*uang elektronik.*)|(.*flazz.*)|(.*kartu tol.*)/ig);
+const regexInstallment = new RegExp(/(.*bayar cicilan.*)|(.*pinjaman.*)|(.*bayar agsuran.*)/ig);
+const regexPBB = new RegExp(/(.*pbb.*)|(.*pajak bumi bangunan.*)|(.*pajak bumi dan bangunan.*)/ig);
+const regexVoucherGame = new RegExp(/(.*cash pb.*)|(.*point blank.*)|(.*genshin impact.*)|(.*genshin.*)|(.*steam wallet.*)|(.*mole.*)|(.*mobile legend.*)|(.*genshin.*)|(.*voucher game.*)/ig);
 
 const textProcessing = (string) => {
-  if (string.test(regexBPJS)) {
-    window.location.assign('https://www.tokopedia.com/tagihan/bpjs-kesehatan/');
-  } else if (string.test(regexPLN)) {
-    window.location.assign('https://www.tokopedia.com/pln/');
-  } else if (string.test(regexPulsa)) {
+	if (regexBPJS.test(string)) {
+		window.location.assign('https://www.tokopedia.com/tagihan/bpjs-kesehatan/');
+	} else if (regexPLN.test(string)) {
+		window.location.assign('https://www.tokopedia.com/pln/');
+	} else if (regexPulsa.test(string)) {
     window.location.assign('https://www.tokopedia.com/pulsa/');
-  } else if (string.test(regexPaketData)) {
+	} else if (regexPaketData.test(string)) {
     window.location.assign('https://www.tokopedia.com/paket-data/');
-  } else if (string.test(regexFlightSchedule)) {
+	} else if (regexFlightSchedule.test(string)) {
     window.location.assign('https://www.tokopedia.com/flight/');
-  } else if (string.test(regexTrainSchedule)) {
+	} else if (regexTrainSchedule.test(string)) {
     window.location.assign('https://tiket.tokopedia.com/kereta-api/');
-  } else if (string.test(regexEmoney)) {
+	} else if (regexEmoney.test(string)) {
     window.location.assign('https://www.tokopedia.com/top-up/emoney/');
-  } else if (string.test(regexInstallment)) {
+	} else if (regexInstallment.test(string)) {
     window.location.assign('https://www.tokopedia.com/angsuran/');
-  } else if (string.test(regexPBB)) {
+	} else if (regexPBB.test(string)) {
     window.location.assign('https://www.tokopedia.com/pajak/pbb/');
-  } else if (string.test(regexVoucherGame)) {
+	} else if (regexVoucherGame.test(string)) {
     window.location.assign('https://www.tokopedia.com/voucher-game/');
   } else {
     // if input doesn't recognize, default state will redirect user to search page
